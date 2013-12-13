170bf2e3a6d8b8122a497fae0687101e95dc64c7
Curwer <niclas.hjalmarsson@gmail.com>
2013-12-13 11:08:29 +0100

    Almost done guys
    Bob

--- src/gui_package/GUI.java
+++ src/gui_package/GUI.java
@@ -2,16 +2,22 @@ package gui_package;
 
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
 import javax.swing.JButton;
+import javax.swing.WindowConstants;
+import javax.swing.text.DefaultCaret;
 
 import client.Client;
 
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.File;
 
 
@@ -29,6 +35,8 @@ public class GUI {
 	private JTextArea chatOutput;
 	private JTextArea userView;
 	
+	JTextPane userInput;
+	
 	
 	/**
 	 * Create the application.
@@ -36,16 +44,21 @@ public class GUI {
 	 */
 	public GUI() {
 		
-		initialize();
+		initialize(null);
 	}
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
-	private void initialize() {
+	public void initialize(KeyEvent e) {
 		frame = new JFrame("Chatroom");
 		frame.setBounds(100, 100, 600, 550);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+		frame.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				close();
+			}
+		});
 		frame.getContentPane().setLayout(null);
 		frame.setVisible(true);
 		
@@ -57,6 +70,8 @@ public class GUI {
 		chatOutput.setEditable(false);
 //		chatOutput.setText(user + " has joined the room!" + System.lineSeparator());
 		scroll_chatOutput.setViewportView(chatOutput);
+		DefaultCaret caret = (DefaultCaret)chatOutput.getCaret();
+		caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
 		
 		
 		JScrollPane scroll_userView = new JScrollPane();
@@ -75,8 +90,12 @@ public class GUI {
 		JButton btnSend = new JButton("Send");
 		btnSend.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				if(userInput.getText().equals("")){
+					chatOutput.append("Writh something pls! // Bob\n");
+				}else {
 				client.send(userInput.getText());
 				userInput.setText("");
+				}
 			}
 		});
 		btnSend.setBounds(10, 403, 89, 23);
@@ -85,14 +104,14 @@ public class GUI {
 		JButton btnLogout = new JButton("Logout");
 		btnLogout.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
-				client.send("/quit");
-				System.exit(1);
+				close();
+		
 			}
 		});
 		btnLogout.setBounds(485, 477, 89, 23);
 		frame.getContentPane().add(btnLogout);
-		
+
+		frame.repaint();
 	}
 	
 	public void addToChatOutput(String input) {
@@ -106,5 +125,12 @@ public class GUI {
 		this.client = client;
 		
 	}
+	
+	private int close(){
+		client.send("/quit");
+		System.exit(1);
+		return 0;
+	}
+	
 
 }
